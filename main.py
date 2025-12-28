448c1fcdb0961fa0dd99b7846acd25cb7b28351c
Ali Ghasemi <a.ghasemi@apk-group.net>
2025-12-28 16:36:02 +0330

    add chapter 3 tasks

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from stats import words_counter, charachters_count
+import sys
+from stats import words_counter, charachters_count, sorted_charachters_count
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 
 def get_book_text(book_path):
@@ -9,10 +14,23 @@ def get_book_text(book_path):
 
 
 def main():
-    book = get_book_text("./books/frankenstein.txt")
-    # print(book)
+    book_path = sys.argv[1]
+    book = get_book_text(book_path)
+    total_words = words_counter(book)
+    charachter_count_dictionary = charachters_count(book)
+    sorted_chars_list = sorted_charachters_count(charachter_count_dictionary)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {words_counter(book)} total words")
-    print(charachters_count(book))
+    print("--------- Character Count -------")
+    # print(charachters_count(book))
+    all_characters_list = sorted_chars_list
+    for ch in all_characters_list:
+        if ch["name"].isalpha():
+            print(ch["name"] + ": " + str(ch["num"]))
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,15 @@ def charachters_count(string):
         else:
             chars[key] = 1
     return chars
+
+
+def sort_on(item):
+    return item["num"]
+
+
+def sorted_charachters_count(chars_dict):
+    chars_list = []
+    for chd in chars_dict:
+        chars_list.append({"name": chd, "num": chars_dict[chd]})
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list
